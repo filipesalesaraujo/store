f854b226cc902d3136d9eb408c2f9337af4efc04
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2024-01-08 14:56:01 -0300

    Add image loading state to ProdutoCard component

--- src/components/produto-card.tsx
+++ src/components/produto-card.tsx
@@ -1,12 +1,19 @@
+'use client';
+
 // Importa as dependências necessárias
 import Image from 'next/image';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from '@/components/ui/button';
 import { ProdutoCardProps } from '@/types/produto';
+import { Skeleton } from './ui/skeleton';
+import { useState } from 'react';
 
 // Define o componente ProdutoCard
 export default function ProdutoCard({ produto, adicionarAoCarrinho, produtoNoCarrinho }: ProdutoCardProps) {
+
 	const isProdutoNoCarrinho = produtoNoCarrinho(produto);
+
+	const [isImageLoaded, setIsImageLoaded] = useState(false);
 
 	return (
 		<Card className='flex flex-col justify-between h-full lg:h-[700px]' role="region" aria-label="Produto">
@@ -15,15 +22,19 @@ export default function ProdutoCard({ produto, adicionarAoCarrinho, produtoNoCar
 					<CardTitle>{produto.nome.length > 53 ? produto.nome.substring(0, 53) + '...' : produto.nome}</CardTitle>
 				</CardHeader>
 				<CardContent className='flex flex-col gap-5'>
-					<div className="h-64 bg-gray-200 relative">
+
+					<div className="h-64 bg-gray-200 relative rounded-md overflow-hidden">
+						{!isImageLoaded && <Skeleton className="h-64 w-full rounded-md" />}
 						<Image
 							layout="fill"
 							src={produto.imagemUrl}
 							alt={produto.nome}
 							objectFit="contain"
 							placeholder="empty"
+							onLoad={() => setIsImageLoaded(true)}
 						/>
 					</div>
+
 					<CardDescription>
 						{produto.descricao.length > 200 ? produto.descricao.substring(0, 200) + '...' : produto.descricao}
 					</CardDescription>
