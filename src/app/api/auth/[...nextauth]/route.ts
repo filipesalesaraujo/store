39124bfda42489dcfc6c9efae90367568a2ee1b3
Filipe Sales Araujo <filipesalesaraujo@gmail.com>
2024-01-12 14:08:25 -0300

    Update GoogleProvider options in route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,8 +6,8 @@ import GoogleProvider from "next-auth/providers/google"
 const options = {
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+            clientId: process.env.GOOGLE_CLIENT_ID || '',
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
         }),
         CredentialsProvider({
             // Nome do provedor de autenticação.
